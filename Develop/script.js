ad5636a3e167baae1697145d889afb50e417dd48
Melissa <melissakalish2@gmail.com>
2023-05-20 14:31:30 -0500

    Final commit for assignment

--- Develop/script.js
+++ Develop/script.js
@@ -7,40 +7,66 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  return;
+  
 
 };
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//These are the differnt variables for the characters used for the password
-var uchar = "abcdefghijklmnopqrstuvwxyz";
-var lchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var nchar = "0123456789";
-var schar = "~!@#$%^&*()_+";
+//These are the differnt variables for the characters that will be used for the password
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = "~!@#$%^&*()_+";
 
 
 // The generatePassword funtion code is below
 function generatePassword() {
-  var password = "";
+  var realPassword = "";
  
-
+    alert("presented with the following prompts for password generating criteria")
  
  //This is the choice of length of a password is between 8 and 128 characters
  var passwordLength = prompt("choose a number between 8 and 128. This will be the password length");
- if (passwordLength === null){
-  if (passwordLength >= 8 && passwordLength <=128){
-
-   
-    alert("in the following prompts, at least one choice of lowercase, uppercase, numeric and/or special characters must be confirmed.");
-
+  if (passwordLength >= 8 && passwordLength <=128) {
+   alert("password must be between 8-128 characters")
  }
+
+       alert("in the following prompts, at least one choice of lowercase, uppercase, numeric and/or special characters must be confirmed.");
+    var user = true;
+    var choices = "";
+
+    var lowerCase = confirm("have your password contain lowercase letters");
+    if (lowerCase === true) {
+      choices += lowerCase;
+      
+    }
+
+    var upperCase = confirm("have your password contain uppercase letters");
+    if (upperCase === true) {
+      choices += upperCase;
+      
+    }
+
+    var numbers = confirm("have your password contain numbers");
+    if (numbers === true) {
+      choices += numbers;
+      
+    }
+
+    var specialCharacters = confirm("have your password contain special characters");
+    if (specialCharacters === true) {
+      choices += specialCharacters
+          }
+
+          //This is the password generation code
+        for (i = 0; i < passwordLength ; i++ ) {
+          realPassword += choices[Math.floor(Math.random()*choices.length)];
  
-  let passwordLength = prompt("enter the length of the password")
+    };
   
-}
-//This is the password generation code
-for (i = 0; i < passwordLength ; i++ ){
-  password += char
-}
+  
+    return realPassword;
+  }
+ 
